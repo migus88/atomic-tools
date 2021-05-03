a076d4b2e87bfef1b2e62d91e7f69b9ef2fd215f
Yura Sokolov <migsil88@gmail.com>
2021-05-03 15:26:27 +0300

    Templates renaming

--- src/Atomic.Injector.Generators/Helpers/Templates/TemplatePaths.cs
+++ src/Atomic.Injector.Generators/Helpers/Templates/TemplatePaths.cs
@@ -3,11 +3,14 @@ namespace Atomic.Injector.Generators.Helpers.Templates
     public static class TemplatePaths
     {
         private const string TemplatesNamespace = "Resources.Templates.";
-        private const string TemplatePrefix = TemplatesNamespace + "Template";
         
-        public const string Container = TemplatePrefix + "Container";
-        public const string PropertyInitialization = TemplatePrefix + "PropertyInitialization";
-        public const string ConstructorInitialization = TemplatePrefix + "ConstructorInitialization";
-        public const string Property = TemplatePrefix + "Property";
+        public const string Container = TemplatesNamespace + "Container";
+        
+        public const string PropertyInitialization = TemplatesNamespace + "PropertyInitialization";
+        public const string ConstructorInitialization = TemplatesNamespace + "ConstructorInitialization";
+        public const string TransientInitialization = TemplatesNamespace + "TransientInitialization";
+        
+        public const string Property = TemplatesNamespace + "Property";
+        public const string TransientProperty = TemplatesNamespace + "TransientProperty";
     }
 }
